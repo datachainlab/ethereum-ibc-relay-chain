8ba3ee90c21fc0959726b35be780fccacdbe0a75
Masanori Yoshida <masanori.yoshida@datachain.jp>
2023-10-12 16:11:55 +0900

    bump yui-relayer up to v0.4.12 and impl ChainConfig::Validate
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- pkg/relay/ethereum/config.go
+++ pkg/relay/ethereum/config.go
@@ -1,6 +1,10 @@
 package ethereum
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/hyperledger-labs/yui-relayer/core"
 )
@@ -9,6 +13,40 @@ var _ core.ChainConfig = (*ChainConfig)(nil)
 
 func (c ChainConfig) Build() (core.Chain, error) {
 	return NewChain(c)
+}
+
+func (c ChainConfig) Validate() error {
+	isEmpty := func(s string) bool {
+		return strings.TrimSpace(s) == ""
+	}
+
+	var errs []error
+	if isEmpty(c.ChainId) {
+		errs = append(errs, fmt.Errorf("config attribute \"chain_id\" is empty"))
+	}
+	if c.EthChainId <= 0 {
+		errs = append(errs, fmt.Errorf("config attribute \"eth_chain_id\" must be greater than zero: %v", c.EthChainId))
+	}
+	if isEmpty(c.RpcAddr) {
+		errs = append(errs, fmt.Errorf("config attribute \"rpc_addr\" is empty"))
+	}
+	if isEmpty(c.HdwMnemonic) {
+		errs = append(errs, fmt.Errorf("config attribute \"hdw_mnemonic\" is empty"))
+	}
+	if isEmpty(c.HdwPath) {
+		errs = append(errs, fmt.Errorf("config attribute \"hdw_path\" is empty"))
+	}
+	if isEmpty(c.IbcAddress) {
+		errs = append(errs, fmt.Errorf("config attribute \"ibc_address\" is empty"))
+	}
+	if c.AverageBlockTimeMsec == 0 {
+		errs = append(errs, fmt.Errorf("config attribute \"average_block_time_msec\" is zero"))
+	}
+	if c.MaxRetryForInclusion == 0 {
+		errs = append(errs, fmt.Errorf("config attribute \"max_retry_for_inclusion\" is zero"))
+	}
+
+	return errors.Join(errs...)
 }
 
 func (c ChainConfig) IBCAddress() common.Address {
