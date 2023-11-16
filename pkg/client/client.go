ba599ea9df1fdb20fd54ad8111b7907da4644a39
Masanori Yoshida <masanori.yoshida@datachain.jp>
2023-11-16 21:01:36 +0900

    impl UnmarshalJSON for callFrame
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"math/big"
 	"time"
@@ -180,6 +181,65 @@ type callFrame struct {
 	// Placed at end on purpose. The RLP will be decoded to 0 instead of
 	// nil if there are non-empty elements after in the struct.
 	Value *big.Int `json:"value,omitempty" rlp:"optional"`
+}
+
+// UnmarshalJSON unmarshals from JSON.
+func (c *callFrame) UnmarshalJSON(input []byte) error {
+	type callFrame0 struct {
+		Type         *vm.OpCode      `json:"-"`
+		From         *common.Address `json:"from"`
+		Gas          *hexutil.Uint64 `json:"gas"`
+		GasUsed      *hexutil.Uint64 `json:"gasUsed"`
+		To           *common.Address `json:"to,omitempty" rlp:"optional"`
+		Input        *hexutil.Bytes  `json:"input" rlp:"optional"`
+		Output       *hexutil.Bytes  `json:"output,omitempty" rlp:"optional"`
+		Error        *string         `json:"error,omitempty" rlp:"optional"`
+		RevertReason *string         `json:"revertReason,omitempty"`
+		Calls        []callFrame     `json:"calls,omitempty" rlp:"optional"`
+		Logs         []callLog       `json:"logs,omitempty" rlp:"optional"`
+		Value        *hexutil.Big    `json:"value,omitempty" rlp:"optional"`
+	}
+	var dec callFrame0
+	if err := json.Unmarshal(input, &dec); err != nil {
+		return err
+	}
+	if dec.Type != nil {
+		c.Type = *dec.Type
+	}
+	if dec.From != nil {
+		c.From = *dec.From
+	}
+	if dec.Gas != nil {
+		c.Gas = uint64(*dec.Gas)
+	}
+	if dec.GasUsed != nil {
+		c.GasUsed = uint64(*dec.GasUsed)
+	}
+	if dec.To != nil {
+		c.To = dec.To
+	}
+	if dec.Input != nil {
+		c.Input = *dec.Input
+	}
+	if dec.Output != nil {
+		c.Output = *dec.Output
+	}
+	if dec.Error != nil {
+		c.Error = *dec.Error
+	}
+	if dec.RevertReason != nil {
+		c.RevertReason = *dec.RevertReason
+	}
+	if dec.Calls != nil {
+		c.Calls = dec.Calls
+	}
+	if dec.Logs != nil {
+		c.Logs = dec.Logs
+	}
+	if dec.Value != nil {
+		c.Value = (*big.Int)(dec.Value)
+	}
+	return nil
 }
 
 func searchRevertReason(result *callFrame) (string, error) {
