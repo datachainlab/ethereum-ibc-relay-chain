f4096fa4321c3f2f983a08bbc245da27126e221b
yoshidan <naohiro.y@gmail.com>
2024-01-02 19:24:59 +0900

    add gas price
    Signed-off-by: yoshidan <naohiro.y@gmail.com>

--- pkg/relay/ethereum/client.go
+++ pkg/relay/ethereum/client.go
@@ -2,6 +2,7 @@ package ethereum
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -18,10 +19,33 @@ func (chain *Chain) CallOpts(ctx context.Context, height int64) *bind.CallOpts {
 	return opts
 }
 
-func (chain *Chain) TxOpts(ctx context.Context) *bind.TransactOpts {
-	return &bind.TransactOpts{
-		From:     chain.signer.Address(),
-		GasLimit: 6382056,
-		Signer:   chain.signer.Sign,
+func (chain *Chain) TxOpts(ctx context.Context) (*bind.TransactOpts, error) {
+	// GasTipCap = min(LimitPriorityFeePerGas, Suggested * PriorityFeeRate)
+	gasTipCap, err := chain.client.SuggestGasTipCap(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to call eth_maxPriorityFeePerGas: %v", err)
 	}
+	chain.config.PriorityFeeRate.Mul(gasTipCap)
+	if l := chain.config.GetLimitPriorityFeePerGas(); l.Sign() > 0 && gasTipCap.Cmp(l) > 0 {
+		gasTipCap = l
+	}
+
+	// GasFeeCap = min(LimitFeePerGas, GasTipCap + BaseFee * BaseFeeRate)
+	head, err := chain.client.HeaderByNumber(ctx, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch the latest header: %v", err)
+	}
+	gasFeeCap := head.BaseFee
+	chain.config.BaseFeeRate.Mul(gasFeeCap)
+	gasFeeCap.Add(gasFeeCap, gasTipCap)
+	if l := chain.config.GetLimitFeePerGas(); l.Sign() > 0 && gasFeeCap.Cmp(l) > 0 {
+		gasFeeCap = l
+	}
+
+	return &bind.TransactOpts{
+		From:      chain.signer.Address(),
+		GasTipCap: gasTipCap,
+		GasFeeCap: gasFeeCap,
+		Signer:    chain.signer.Sign,
+	}, nil
 }
