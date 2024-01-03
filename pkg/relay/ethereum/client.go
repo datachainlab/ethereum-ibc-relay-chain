9ce0eb0efe7348d0eb452ce3b893d84ab7de04bf
yoshidan <naohiro.y@gmail.com>
2024-01-03 22:29:47 +0900

    use eth_feeHistory
    Signed-off-by: yoshidan <naohiro.y@gmail.com>

--- pkg/relay/ethereum/client.go
+++ pkg/relay/ethereum/client.go
@@ -20,32 +20,58 @@ func (chain *Chain) CallOpts(ctx context.Context, height int64) *bind.CallOpts {
 }
 
 func (chain *Chain) TxOpts(ctx context.Context) (*bind.TransactOpts, error) {
-	// GasTipCap = min(LimitPriorityFeePerGas, Suggested * PriorityFeeRate)
-	gasTipCap, err := chain.client.SuggestGasTipCap(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to call eth_maxPriorityFeePerGas: %v", err)
+	txOpts := &bind.TransactOpts{
+		From:   chain.signer.Address(),
+		Signer: chain.signer.Sign,
 	}
+	if chain.config.EnableLegacyTx {
+		gasPrice, err := chain.Client().SuggestGasPrice(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to suggest gas price: %v", err)
+		}
+		txOpts.GasPrice = gasPrice
+		return txOpts, nil
+	}
+
+	gasTipCap, gasFeeCap, err := chain.feeHistory(ctx, float64(chain.Config().RewardPercentile))
+	if err != nil {
+		return nil, err
+	}
+	// GasTipCap = min(LimitPriorityFeePerGas, Suggested * PriorityFeeRate)
 	chain.config.PriorityFeeRate.Mul(gasTipCap)
 	if l := chain.config.GetLimitPriorityFeePerGas(); l.Sign() > 0 && gasTipCap.Cmp(l) > 0 {
 		gasTipCap = l
 	}
-
 	// GasFeeCap = min(LimitFeePerGas, GasTipCap + BaseFee * BaseFeeRate)
-	head, err := chain.client.HeaderByNumber(ctx, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch the latest header: %v", err)
-	}
-	gasFeeCap := head.BaseFee
 	chain.config.BaseFeeRate.Mul(gasFeeCap)
 	gasFeeCap.Add(gasFeeCap, gasTipCap)
 	if l := chain.config.GetLimitFeePerGas(); l.Sign() > 0 && gasFeeCap.Cmp(l) > 0 {
 		gasFeeCap = l
 	}
 
-	return &bind.TransactOpts{
-		From:      chain.signer.Address(),
-		GasTipCap: gasTipCap,
-		GasFeeCap: gasFeeCap,
-		Signer:    chain.signer.Sign,
-	}, nil
+	if gasFeeCap.Cmp(gasTipCap) < 0 {
+		return nil, fmt.Errorf("maxFeePerGas (%v) < maxPriorityFeePerGas (%v)", gasFeeCap, gasTipCap)
+	}
+	txOpts.GasFeeCap = gasFeeCap
+	txOpts.GasTipCap = gasTipCap
+	return txOpts, nil
+}
+
+func (chain *Chain) feeHistory(ctx context.Context, rewardPercentile float64) (*big.Int, *big.Int, error) {
+	history, err := chain.Client().FeeHistory(ctx, 1, nil, []float64{rewardPercentile})
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to get feeHistory: %v", err)
+	}
+	if len(history.Reward) == 0 {
+		return nil, nil, fmt.Errorf("no reward found")
+	}
+	if len(history.Reward[0]) == 0 {
+		return nil, nil, fmt.Errorf("no reward found")
+	}
+	if len(history.BaseFee) < 2 {
+		return nil, nil, fmt.Errorf("insufficient base fee")
+	}
+	gasTipCap := history.Reward[0][0]
+	gasFeeCap := history.BaseFee[1]
+	return gasTipCap, gasFeeCap, nil
 }
