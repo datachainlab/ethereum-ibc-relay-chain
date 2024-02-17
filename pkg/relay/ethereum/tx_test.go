09f6ec51362adca83037cc1f0b2e579e78b63a04
Dongri Jin <dongri.jin@speee.jp>
2024-02-17 22:49:12 +0900

    Add test case
    Signed-off-by: Dongri Jin <dongri.jin@speee.jp>

--- pkg/relay/ethereum/tx_test.go
+++ pkg/relay/ethereum/tx_test.go
@@ -2,6 +2,7 @@ package ethereum
 
 import (
 	"encoding/hex"
+	"math/big"
 	"strings"
 	"testing"
 
@@ -39,6 +40,38 @@ func TestRevertReasonParserAddedCustomError(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "Error(string)", s)
 	require.Equal(t, []interface{}{"Not enough Ether provided."}, args)
+}
+
+func TestRevertReasonParserCustomError(t *testing.T) {
+	customErrors := []*CustomError{
+		{
+			FunctionName: "AppError",
+			Arguments: []*Argument{
+				{
+					Type: "string",
+				},
+			},
+		},
+		{
+			FunctionName: "InsufficientBalance",
+			Arguments: []*Argument{
+				{
+					Type: "uint256",
+				},
+				{
+					Type: "uint256",
+				},
+			},
+		},
+	}
+
+	erepo := NewErrorsRepository(customErrors)
+	s, args, err := erepo.ParseError(
+		hexToBytes("0xcf47918100000000000000000000000000000000000000000000000000000000000000070000000000000000000000000000000000000000000000000000000000000009"),
+	)
+	require.NoError(t, err)
+	require.Equal(t, "InsufficientBalance(uint256,uint256)", s)
+	require.Equal(t, []interface{}{big.NewInt(7), big.NewInt(9)}, args)
 }
 
 func hexToBytes(s string) []byte {
